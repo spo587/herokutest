7bb006eb76aa23b3e71eeea312a1b9313fc54663
spo587 <samueljpost@gmail.com>
2015-05-11 01:11:53 -0400

    added port listening info

--- index.js
+++ index.js
@@ -11,4 +11,10 @@ app.get('/', function(req, res){
     var file = fs.createReadStream('./set.html');
     file.pipe(res);
 
-}).listen(8080)
+});
+
+var port = process.env.PORT || 8080;
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
